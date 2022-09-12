9a24a6b854f8afdb3ae739f69bfae15a5c1e4912
huangzengfu <516893261@qq.com>
2022-09-12 23:55:24 +0800

    20220912commit文件修改后提交测试

--- test/src/main/YYC.java
+++ test/src/main/YYC.java
@@ -4,5 +4,6 @@ public class YYC {
     public static void main(String[] args) {
         System.out.println("hf love yyc!");
         System.out.println("hf love yyc!");
+        System.out.println("hf love yyc!");
     }
 }
